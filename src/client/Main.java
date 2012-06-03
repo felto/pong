71e238399b10c27c5ae4acc45fcb97b8c947fb48
plmTorus <perlinman@gmail.com>
2012-06-04 00:37:59 +0300

    Stay happy.

--- src/client/Main.java
+++ src/client/Main.java
@@ -4,5 +4,28 @@ public class Main {
 	public static void main(String args[]) {
 		System.out.println("YOU GIT!");
 		System.out.println("NO, YOU'RE A GIT!");
+		
+		final BufferedImage img = ImageIO.read(new URL("http://www.baconbabble.com/wp-content/uploads/2011/05/smile-1.jpg"));
+		JFrame jF = new JFrame(":)");
+		
+		jF.add(new JPanel() {
+			private static final long serialVersionUID = 3303264469218221735L;
+
+			@Override
+			public void paint(Graphics g) {
+				g.drawImage(img, 0, 0, new ImageObserver() {
+					@Override
+					public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
+						return true;
+					}
+				});
+			}
+		});
+		
+		jF.setResizable(false);
+		jF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jF.setPreferredSize(new Dimension(img.getWidth(), img.getHeight()));
+		jF.pack();
+		jF.setVisible(true);
 	}
 }
